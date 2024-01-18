2edb845008c88fcec5b27fbd2d4ee56f8cb2aef6
SHARAD INGLE <122912274+Sharad2124Ingle@users.noreply.github.com>
2024-01-18 15:42:59 +0530

    Add files via upload

---
+++ spanpr/accounts/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# user - sharad pass - Sharli@123
+
+class User(AbstractUser):
+    is_staff= models.BooleanField('Is admin', default=False)
+    is_employee = models.BooleanField('Is employee', default=True)
+
+
+
+class Record(models.Model):
+    myuser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='myuser_records')
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=10)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10) 
+    mall_id = models.CharField(max_length=15) 
+
+    def __str__(self):
+        return f"Record ID: {self.id}, myuser: {self.myuser}"
+        #return self.user.username
+
+    def save(self, *args, **kwargs):
+        if self.myuser:  # Make sure myuser is set before saving
+            user_email = self.myuser.email
+            self.email = user_email  # Populate the email field with the user's email
+            # user_phone = self.myuser.phone
+            # self.phone = user_phone
+            # if not self.phone:
+            #     self.phone = user_phone 
+            # Update related User instance's first_name and last_name
+            self.myuser.first_name = self.first_name
+            self.myuser.last_name = self.last_name
+            self.myuser.save()
+
+        super().save(*args, **kwargs)
---
+++ spanpr/accounts/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import *
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+
+def home(request):
+    # Check if user is authenticated before filtering records
+    if request.user.is_authenticated:
+        records = Record.objects.all
+    else:
+        records = None
+
+    return render(request, 'home.html', {'records': records})
+
+def login_user(request):
+    form = LoginForm(request.POST or None)
+    msg = None
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                # Check if the user is active and not anonymous
+                if user.is_active and not user.is_anonymous:
+                    login(request, user)
+                    messages.success(request, "Welcome!")
+                    return redirect('home')
+                else:
+                    messages.error(request, "Your account is not active.")
+            else:
+                messages.error(request, "Invalid credentials. Please try again or sign up.")
+        else:
+            messages.error(request, "Invalid credentials. Please try again or sign up.")
+            return redirect('home')
+
+    return render(request, 'loginpage.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been successfully logged out.")
+    return redirect('login_page')
+
+def register(request):
+    msg = None
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            msg = 'user created'
+            messages.success(request, "account created, please login.")
+            return redirect('login_page')
+        else:
+            msg = 'form is not valid'
+    else:
+        form = SignUpForm()
+    return render(request,'register.html', {'form': form, 'msg': msg})
+
+@login_required
+def add_record(request):
+    messages.success(request, "you can add a record below")
+
+    if request.method == 'POST':
+        form = AddRecordForm(request.POST)
+        if form.is_valid():
+            # Get the current user/customer
+            myuser = request.user
+            # Set the customer for the record before saving
+            record = form.save(commit=False)
+            record.myuser = myuser
+            record.save()
+            return redirect('home')
+    else:
+        form = AddRecordForm()
+
+    return render(request, 'add_record.html', {'form': form})
+
+
+
+
